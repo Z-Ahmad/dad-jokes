207d6fcc930823da79ddb280f5983a135176298f
Z-Ahmad <zakiahmad42661@gmail.com>
2021-06-06 17:07:09 -0400

    added a footer and a joke limit

--- app.js
+++ app.js
@@ -36,10 +36,14 @@ const images = [
 const darkToggle = document.querySelector(".toggleDark")
 const body = document.querySelector("body")
 const card = document.querySelector(".card")
+const footer = document.querySelector(".footer")
 const jokeImg = document.querySelector(".img")
 const jokeText = document.querySelector(".jokeText")
 const jokeBtn = document.querySelector(".jokeBtn")
-const header = {}
+const apiLink = (document.createElement("a").href =
+  "https://icanhazdadjoke.com/")
+let requests = 0
+const limit = 50
 
 const fetchJoke = async () => {
   const jokeObject = await (
@@ -63,14 +67,25 @@ const updateJoke = (text) => {
   jokeImg.src = images[Math.floor(Math.random() * 7)].src
 }
 
+const limitReached = () => {
+  jokeText.innerHTML = `That's enough! Go to the 
+    <a href=${apiLink}>icanhazdadjoke</a>
+   site for more jokes!`
+}
+
 jokeBtn.addEventListener("click", async () => {
-  try {
-    const jokeObject = await fetchJoke()
-    const jokeText = jokeObject.joke
-    updateJoke(jokeText)
-    // console.log(jokeText)
-  } catch {
-    error()
+  if (requests <= limit) {
+    try {
+      const jokeObject = await fetchJoke()
+      const jokeText = jokeObject.joke
+      updateJoke(jokeText)
+      // console.log(jokeText)
+    } catch {
+      error()
+    }
+    requests++
+  } else {
+    limitReached()
   }
 })
 
@@ -78,4 +93,5 @@ darkToggle.addEventListener("click", () => {
   body.classList.toggle("bodyDark")
   card.classList.toggle("cardDark")
   jokeBtn.classList.toggle("jokeBtnDark")
+  footer.classList.toggle("footerDark")
 })
