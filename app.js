d95df9046d89144497f2814e6fba62975fdd4302
Z-Ahmad <zakiahmad42661@gmail.com>
2021-06-12 11:50:15 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,37 +1,4 @@
-const images = [
-  {
-    id: 1,
-    src: "https://images.unsplash.com/photo-1544507888-56d73eb6046e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  },
-  {
-    id: 2,
-    src: "https://images.unsplash.com/photo-1586474714722-d1f9e551cf34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  },
-  {
-    id: 3,
-    src: "https://images.unsplash.com/photo-1577202214328-c04b77cefb5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
-  },
-  {
-    id: 4,
-    src: "https://images.unsplash.com/photo-1499081589563-7c400fcd94e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80",
-  },
-  {
-    id: 5,
-    src: "https://images.unsplash.com/photo-1603987248955-9c142c5ae89b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  },
-  {
-    id: 6,
-    src: "https://images.unsplash.com/photo-1531923610693-c816870f3b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  },
-  {
-    id: 7,
-    src: "https://images.unsplash.com/photo-1603610515737-193e0b423983?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  },
-  {
-    id: 8,
-    src: "https://images.unsplash.com/photo-1606823616058-541d59dadcb2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
-  },
-]
+// console.log(images)
 
 const darkToggle = document.querySelector(".toggleDark")
 const body = document.querySelector("body")
@@ -75,7 +42,6 @@ const limitReached = () => {
    site for more jokes!`
   jokeImg.src = stop
   jokeBtn.disabled = true
-  jokeBtn
 }
 
 jokeBtn.addEventListener("click", async () => {
